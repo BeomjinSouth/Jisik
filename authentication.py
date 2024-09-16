47847c55dba434ccbede66e6ede395637e1e0394
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2024-09-16 23:27:38 +0900

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -5,13 +5,12 @@ from yaml.loader import SafeLoader
 import hashlib
 from email_validator import validate_email, EmailNotValidError
 
-# 사용자 정보를 저장할 users.yaml 파일을 로드하거나 생성합니다.
 def load_user_config():
     try:
         with open('users.yaml') as file:
             config = yaml.load(file, Loader=SafeLoader)
     except FileNotFoundError:
-        config = {'credentials': {'usernames': {}}}
+        config = {'credentials': {'usernames': {}, 'names': {}, 'passwords': []}}
     return config
 
 def save_user_config(config):
@@ -24,7 +23,6 @@ def register_user():
     password = st.text_input('비밀번호', type='password')
     password_confirm = st.text_input('비밀번호 확인', type='password')
     if st.button('계정 등록'):
-        # 이메일 유효성 검사
         try:
             validate_email(email)
         except EmailNotValidError as e:
@@ -38,12 +36,12 @@ def register_user():
             st.error('이미 등록된 이메일입니다.')
             return
         hashed_password = stauth.Hasher([password]).generate()[0]
-        config['credentials']['usernames'][email] = {
-            'email': email,
-            'password': hashed_password
-        }
+        config['credentials']['usernames'][email] = email
+        config['credentials']['names'][email] = email.split('@')[0]  # 이름을 이메일 앞부분으로 설정
+        config['credentials']['passwords'].append(hashed_password)
         save_user_config(config)
         st.success('계정이 성공적으로 생성되었습니다. 로그인 해주세요.')
+
 def login_user():
     st.subheader('로그인')
     email = st.text_input('이메일 주소')
@@ -53,15 +51,21 @@ def login_user():
         if email not in config['credentials']['usernames']:
             st.error('등록되지 않은 이메일입니다.')
             return
-        hashed_password = config['credentials']['usernames'][email]['password']
+        index = list(config['credentials']['usernames']).index(email)
+        hashed_password = config['credentials']['passwords'][index]
+        names = list(config['credentials']['names'].values())
+        usernames = list(config['credentials']['usernames'].values())
+        passwords = config['credentials']['passwords']
         authenticator = stauth.Authenticate(
-            {'usernames': {email: {'password': hashed_password}}},
+            names,
+            usernames,
+            passwords,
             'cookie_name',
             'signature_key',
             cookie_expiry_days=30
         )
-        # 매개변수를 위치 인자로 전달하고, 반환값을 두 개로 받습니다.
-        name, authentication_status = authenticator.login('Login', 'main')
+        # 반환값을 세 개로 받습니다.
+        name, authentication_status, username = authenticator.login('Login', 'main')
         if authentication_status:
             st.session_state['user'] = email
             st.success(f'환영합니다, {email}님!')
@@ -70,4 +74,3 @@ def login_user():
             st.error('이메일 또는 비밀번호가 일치하지 않습니다.')
         elif authentication_status == None:
             st.warning('로그인 정보를 입력해주세요.')
-
