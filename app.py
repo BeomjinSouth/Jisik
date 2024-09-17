8c47e1191fb716527499a044b82db45559db9a2a
BeomjinSouth <159209289+BeomjinSouth@users.noreply.github.com>
2024-09-18 01:49:07 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 import streamlit as st
-from authentication import register_user, login_user
+from authentication import login_user, register_user
 from question_generation import generate_questions
 from chat_interface import chat_interface
 
 def main():
     st.title('AI 학습 플랫폼')
-    if 'user' not in st.session_state:
-        menu = ['로그인', '회원가입']
-        choice = st.sidebar.selectbox('메뉴', menu)
-        if choice == '로그인':
-            login_user()
-        elif choice == '회원가입':
-            register_user()
+    menu = ['로그인', '회원가입']
+    choice = st.sidebar.selectbox('메뉴', menu)
+    if choice == '로그인':
+        login_user()
+    elif choice == '회원가입':
+        register_user()
     else:
-        st.sidebar.success(f"로그인됨: {st.session_state['user']}")
-        menu = ['문제 생성', '채팅', '로그아웃']
-        choice = st.sidebar.selectbox('메뉴', menu)
-        if choice == '문제 생성':
-            generate_questions()
-        elif choice == '채팅':
-            chat_interface()
-        elif choice == '로그아웃':
-            st.session_state.pop('user')
-            st.experimental_rerun()
+        st.subheader('메인 페이지')
+
 
 if __name__ == '__main__':
     main()
--- authentication.py
+++ authentication.py
@@ -1,21 +1,17 @@
 import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
-from yaml.loader import SafeLoader
-import hashlib
+import bcrypt
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from email_validator import validate_email, EmailNotValidError
 
-def load_user_config():
-    try:
-        with open('users.yaml') as file:
-            config = yaml.load(file, Loader=SafeLoader)
-    except FileNotFoundError:
-        config = {'credentials': {'usernames': {}, 'names': {}, 'passwords': []}}
-    return config
 
-def save_user_config(config):
-    with open('users.yaml', 'w') as file:
-        yaml.dump(config, file)
+def get_worksheet():
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('service_account.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open_by_key('157IwlgSWvzxbqPoKZwTiRACc6kn1gh1EUOf3SI-JTyw')
+    worksheet = sheet.sheet1  # 첫 번째 시트 사용
+    return worksheet
 
 def register_user():
     st.subheader('회원가입')
@@ -23,23 +19,28 @@ def register_user():
     password = st.text_input('비밀번호', type='password')
     password_confirm = st.text_input('비밀번호 확인', type='password')
     if st.button('계정 등록'):
+        # 이메일 유효성 검사
         try:
             validate_email(email)
-        except EmailNotValidError as e:
+        except EmailNotValidError:
             st.error('유효한 이메일 주소를 입력해주세요.')
             return
         if password != password_confirm:
             st.error('비밀번호가 일치하지 않습니다.')
             return
-        config = load_user_config()
-        if email in config['credentials']['usernames']:
+        
+        # 스프레드시트에서 이메일 중복 체크
+        worksheet = get_worksheet()
+        emails = worksheet.col_values(1)  # A열의 이메일 목록
+        if email in emails:
             st.error('이미 등록된 이메일입니다.')
             return
-        hashed_password = stauth.Hasher([password]).generate()[0]
-        config['credentials']['usernames'][email] = email
-        config['credentials']['names'][email] = email.split('@')[0]  # 이름을 이메일 앞부분으로 설정
-        config['credentials']['passwords'].append(hashed_password)
-        save_user_config(config)
+        
+        # 비밀번호 해싱
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        
+        # 스프레드시트에 사용자 정보 저장
+        worksheet.append_row([email, hashed_password.decode('utf-8')])
         st.success('계정이 성공적으로 생성되었습니다. 로그인 해주세요.')
 
 def login_user():
@@ -47,30 +48,19 @@ def login_user():
     email = st.text_input('이메일 주소')
     password = st.text_input('비밀번호', type='password')
     if st.button('로그인'):
-        config = load_user_config()
-        if email not in config['credentials']['usernames']:
+        worksheet = get_worksheet()
+        records = worksheet.get_all_records()
+        user_found = False
+        for record in records:
+            if record['Email'] == email:
+                user_found = True
+                stored_password = record['Password'].encode('utf-8')
+                if bcrypt.checkpw(password.encode('utf-8'), stored_password):
+                    st.success(f'환영합니다, {email}님!')
+                    st.session_state['user'] = email
+                    st.experimental_rerun()
+                else:
+                    st.error('비밀번호가 일치하지 않습니다.')
+                break
+        if not user_found:
             st.error('등록되지 않은 이메일입니다.')
-            return
-        index = list(config['credentials']['usernames']).index(email)
-        hashed_password = config['credentials']['passwords'][index]
-        names = list(config['credentials']['names'].values())
-        usernames = list(config['credentials']['usernames'].values())
-        passwords = config['credentials']['passwords']
-        authenticator = stauth.Authenticate(
-            names,
-            usernames,
-            passwords,
-            'cookie_name',
-            'signature_key',
-            cookie_expiry_days=30
-        )
-        # 반환값을 세 개로 받습니다.
-        name, authentication_status, username = authenticator.login('Login', 'main')
-        if authentication_status:
-            st.session_state['user'] = email
-            st.success(f'환영합니다, {email}님!')
-            st.experimental_rerun()
-        elif authentication_status == False:
-            st.error('이메일 또는 비밀번호가 일치하지 않습니다.')
-        elif authentication_status == None:
-            st.warning('로그인 정보를 입력해주세요.')
